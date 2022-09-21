e49751121923819494fdc775583df4f083b26b83
Sean Culliton <smculliton@gmail.com>
2022-09-21 11:24:08 -0600

    Complete translate braille and braille row methods

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,6 +1,10 @@
+require './lib/braille_library'
+
 class NightWriter
   attr_reader :message_path, :write_path, :message
-  
+
+  include BrailleLibrary
+
   def initialize(hash = { message_path: ARGV[0], write_path: ARGV[1] } )
     @message_path = hash[:message_path]
     @write_path = hash[:write_path]
@@ -13,5 +17,15 @@ class NightWriter
 
   def open_file(filepath)
     @message = File.open(filepath).read.chomp
+  end
+
+  def translate_braille(string)
+    translation = ''
+    3.times { |i| translation.concat("#{braille_row(string, i)}\n") }
+    translation.chomp
+  end
+
+  def braille_row(string, row)
+    string.chars.map { |letter| braille_library[letter][row] }.join
   end
 end
