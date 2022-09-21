62ad2dcad465ec77704e1523cfb11245c6ae6287
Sean Culliton <smculliton@gmail.com>
2022-09-20 22:59:50 -0600

    Create and pass initialize tests

---
+++ lib/night_writer.rb
@@ -0,0 +1,17 @@
+class NightWriter
+  attr_reader :message_path, :write_path, :message 
+  
+  def initialize(hash = { message_path: ARGV[0], write_path: ARGV[1] } )
+    @message_path = hash[:message_path]
+    @write_path = hash[:write_path]
+    @message = ''
+  end
+
+  def character_statement
+    "Created 'braille.txt' containt 256 characters"
+  end
+
+  def open_file(filepath)
+    message = File.open(filepath)
+  end
+end
---
+++ spec/night_writer_spec.rb
@@ -0,0 +1,36 @@
+require 'rspec'
+require './lib/night_writer'
+
+RSpec.describe NightWriter do
+  let(:night_writer) { NightWriter.new({ message_path: 'message.txt', write_path: 'braille.txt' }) }
+
+  describe '#initialize' do
+    it 'exists' do 
+      expect(night_writer).to be_a NightWriter 
+    end
+    it 'has a message file path' do 
+      # allow(NightWriter).to receive(:ARGV[0]).and_return('message.txt')
+      expect(night_writer.message_path).to eq('message.txt')
+    end
+    it 'has a file path to write to' do 
+      # allow(NightWriter).to receive(:ARGV[0]).and_return('braille.txt')
+      expect(night_writer.write_path).to eq('braille.txt')
+    end
+    it 'starts with no message' do 
+      expect(night_writer.message).to eq('')
+    end
+  end
+
+  describe '#character_statement' do 
+    it 'writes a message' do 
+      expect(night_writer.character_statement).to eq("Created 'braille.txt' containt 256 characters")
+    end
+  end
+
+  describe '#open_file' do 
+    it 'opens a file' do 
+      expect(night_writer.open_file(night_writer.message_path)).to be_a File
+    end
+  end
+
+end
