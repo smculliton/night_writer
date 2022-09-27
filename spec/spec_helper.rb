0a656418b7239811ca83c8f8a1b8a8ad206c37ef
Sean Culliton <smculliton@gmail.com>
2022-09-26 21:58:29 -0600

    Add spec files to spec helper get 100% simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,10 @@
 require 'simplecov'
 SimpleCov.start
 
-require_relative 'braille_reader_spec'
-require_relative 'braille_writer_spec'
+require_relative 'braille_message_spec'
+require_relative 'braille_translator_spec'
+require_relative 'character_spec'
+require_relative 'file_io_spec'
+require_relative 'formattable_spec'
+require_relative 'message_spec'
+require_relative 'translator_spec'
